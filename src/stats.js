ff6a31501f8fb4bd8bcf594205670b4495580d70
szapataq <szapata013@gmail.com>
2020-06-16 10:04:53 -0500

    Implementacion de funcion unique y broken

--- src/stats.js
+++ src/stats.js
@@ -6,40 +6,40 @@ const unique = (arrayObjLinks) => {
 
 const broken = (arrayObjLinks) => arrayObjLinks.filter((link) => link.status >= 404).length;
 
-const array = [
-  {
-    href: 'https://es.wikipedia.org/wiki/Markdown',
-    text: 'Markdown',
-    path: 'D:\\LABORATORIA\\LIM012-fe-md-links\\test_example\\directory1\\file1_1.md',
-    status: 200,
-    statusText: 'OK',
-  },
-  {
-    href: 'https://nodejs.org/',
-    text: 'Node.js',
-    path: 'D:\\LABORATORIA\\LIM012-fe-md-links\\test_example\\directory1\\file1_1.md',
-    status: 200,
-    statusText: 'OK',
-  },
-  {
-    href: 'https://developers.google.com/v8/',
-    text: 'motor de JavaScript V8 de Chrome',
-    path: 'D:\\LABORATORIA\\LIM012-fe-md-links\\test_example\\file1.md',
-    status: 404,
-    statusText: 'OK',
-  },
-  {
-    href: 'https://nodejs.org/',
-    text: 'Node.js',
-    path: 'D:\\LABORATORIA\\LIM012-fe-md-links\\test_example\\file1.md',
-    status: 200,
-    statusText: 'OK',
-  },
-];
+// const array = [
+//   {
+//     href: 'https://es.wikipedia.org/wiki/Markdown',
+//     text: 'Markdown',
+//     path: 'D:\\LABORATORIA\\LIM012-fe-md-links\\test_example\\directory1\\file1_1.md',
+//     status: 200,
+//     statusText: 'OK',
+//   },
+//   {
+//     href: 'https://nodejs.org/',
+//     text: 'Node.js',
+//     path: 'D:\\LABORATORIA\\LIM012-fe-md-links\\test_example\\directory1\\file1_1.md',
+//     status: 200,
+//     statusText: 'OK',
+//   },
+//   {
+//     href: 'https://developers.google.com/v8/',
+//     text: 'motor de JavaScript V8 de Chrome',
+//     path: 'D:\\LABORATORIA\\LIM012-fe-md-links\\test_example\\file1.md',
+//     status: 404,
+//     statusText: 'OK',
+//   },
+//   {
+//     href: 'https://nodejs.org/',
+//     text: 'Node.js',
+//     path: 'D:\\LABORATORIA\\LIM012-fe-md-links\\test_example\\file1.md',
+//     status: 200,
+//     statusText: 'OK',
+//   },
+// ];
 
-console.log('Total:', array.length);
-console.log('Broken:', broken(array));
-console.log('Unique:', unique(array));
+// console.log('Total:', array.length);
+// console.log('Broken:', broken(array));
+// console.log('Unique:', unique(array));
 
 module.exports = {
   unique,
