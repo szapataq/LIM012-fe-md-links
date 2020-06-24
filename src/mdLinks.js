4d1b932f612bde7817fffc9111cf9eb5b6956b37
szapataq <szapata013@gmail.com>
2020-06-24 08:22:27 -0500

    adicion de reject

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,17 +1,19 @@
 const route = require('./main.js');
 const validate = require('./validate.js');
 
-const mdLinks = (inputPath, options = { validate: false }) => new Promise((resolve) => {
+const mdLinks = (inputPath, options = { validate: false }) => new Promise((resolve, reject) => {
   if (route.isValidRoute(inputPath)) {
     if (options.validate === true) {
       resolve(validate.validateLinks(inputPath));
     } else {
       resolve(route.getLinksMd(inputPath));
     }
+  } else {
+    reject(new Error('FAIL'));
   }
 });
 
 // console.log(mdLinks('./test_example', { validate: true }));
-// mdLinks('./test_example', { validate: true }).then((res) => console.log(res));
+// mdLinks('./test_example', { validate: false }).then((res) => console.log(res));
 
 module.exports = { mdLinks };
